16577de2398d952723b6735a80f88cef02b4c786
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-07-04 10:27:23 -0400

    Se implementa un rootpage con el titulo del program y un boton llamado import folder que lleva a la pagina MainPage que no está implementada aún.

---
+++ lib/src/RootPage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import './MainPage.dart';
+
+class RootPage extends StatelessWidget {
+  const RootPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Nastid'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Nastid',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainPage()),
+                );
+              },
+              child: const Text('Import File'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
