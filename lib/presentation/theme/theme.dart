cea72ec29f7821315ee6d5412707cc94bfaf6f81
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-07-09 13:26:45 -0400

    Creacion de theme.dart con un tema claro y tema oscuro, no están configurados.

---
+++ lib/presentation/theme/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyAppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primarySwatch: Colors.orange
+    );
+
+  static ThemeData darkTheme = ThemeData(
+    primarySwatch: Colors.purple
+    );
+}
