2235a39db1ef71e4f46c0143aab45cf363758c35
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-07-14 14:28:12 -0400

    PathFolderImagesCubit tiene un metodo para setear un nuevo path

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
-import './presentation/theme/light_theme/light_theme.dart';
+import 'config/theme/light_theme/light_theme.dart';
 import './config/router/router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import './presentation/blocs/blocs.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+class BlocsProviders extends StatelessWidget{ 
+  const BlocsProviders({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PathFolderImagesCubit(),
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/presentation/blocs/identifier/path_folder_images_cubit.dart
+++ lib/presentation/blocs/identifier/path_folder_images_cubit.dart
@@ -1,2 +1,9 @@
+import 'package:bloc/bloc.dart';
 
+class PathFolderImagesCubit extends Cubit<String> {
+  PathFolderImagesCubit(): super('No-Images-Path');
 
+  void setPathFolder(pathFolder) {
+    emit(pathFolder);
+  }
+}
