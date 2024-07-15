c4e83b5e468236656fcddb3f3af013f5984b99d2
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-07-14 21:31:26 -0400

    ImageFileModel  modificacion para usar equatable.

--- lib/config/utils/models.dart
+++ lib/config/utils/models.dart
@@ -1,8 +1,11 @@
+import 'package:equatable/equatable.dart';
 
-
-class ImageFileModel {
+class ImageFileModel extends Equatable {
   final String name;
   final String path;
 
-  ImageFileModel({required this.name, required this.path});
+  const ImageFileModel({required this.name, required this.path});
+
+  @override
+  List<Object?> get props => [name, path];
 }
