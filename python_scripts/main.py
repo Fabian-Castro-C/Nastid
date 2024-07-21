402cffb4ffe7fef40071f39aea7f8dd39f7fc55d
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-07-21 18:45:42 -0400

    La api ahora en vez de retornar la matriz de datos retorna un png.

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -4,7 +4,11 @@ import os
 sys.path.append(os.path.dirname(__file__))
 
 
+import numpy as np
+import matplotlib.pyplot as plt
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from io import BytesIO
 import matrix as mx
 
 
@@ -15,6 +19,18 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/matrix")
-def random_matrix():
+def random_matrix() -> dict:
+    
     matrix = mx.random_matrix(512)
-    return {"matrix": matrix.tolist()}
+
+    # Convertir matriz a imagen
+    fig, ax = plt.subplots(figsize=(5, 5), dpi=80)
+    ax.imshow(matrix, cmap='gray')
+    ax.axis('off')  # Ocultar los ejes
+
+    # Guardar la imagen en un buffer de bytes
+    buf = BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+    buf.seek(0)
+
+    return StreamingResponse(buf, media_type="image/png")
