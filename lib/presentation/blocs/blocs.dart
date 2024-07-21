b24ab595b4657075919139bbcd7049e3f8830c95
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2024-07-21 17:18:20 -0400

    Exports de los blocs generados

--- lib/presentation/blocs/blocs.dart
+++ lib/presentation/blocs/blocs.dart
@@ -1,2 +1,4 @@
 export './identifier/file_selection_cubit/file_selection_cubit.dart';
 export './identifier/file_selection_cubit/file_selection_state.dart';
+export './identifier/matrix_cubit/matrix_cubit.dart';
+export './identifier/matrix_cubit/matrix_state.dart';
